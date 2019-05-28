fe745166d401de5e9eb8a9db7d5b450033142ff3
Sonata <sonata.cernikaite@gmail.com>
2019-05-28 16:58:20 +0300

    Home work 14

---
+++ homework_14/14b.js
@@ -0,0 +1,11 @@
+var mas = ['2019-05-28 14:40', 'ABH785', 5000, 118];
+document.getElementById('main').innerHTML = mas[0];
+document.getElementById('main1').innerHTML = mas[1];
+document.getElementById('main2').innerHTML = mas[2];
+document.getElementById('main3').innerHTML = mas[3];
+
+function aa() {
+    var s = (mas[2]/1000)/(mas[3]/120);
+    return s;
+}
+document.getElementById('kmh').innerHTML = aa().toFixed(0);
