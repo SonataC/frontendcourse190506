920fc176206218528bba0163ffa5fe6a4a4a451c
Sonata <sonata.cernikaite@gmail.com>
2019-06-12 14:40:19 +0300

    Home work 20 fix
    Added changes to JS file

--- homework_16/16.js
+++ homework_16/16.js
@@ -1,14 +1,14 @@
-var click = 0;
-function color(_this) {
-    if (click === 0) {
-        _this.style.backgroundColor = "green";
-        _this.style.borderRadius = '50%';
-        _this.style.transition = '1s';
-        return click += 1;
-    } else if (click === 1) {
-        _this.style.backgroundColor = "red";
-        _this.style.borderRadius = '0';
-        return click -= 1;
-    }
+var CLASS_ROUND = 'round';
 
+var bool = false;
+function color(event, _this) {
+    event.preventDefault();
+    if (!bool) {
+        _this.classList.add(CLASS_ROUND);
+        bool = true;
+    } else {
+        _this.classList.remove(CLASS_ROUND);
+        bool = false;
+    }
+    return false;
 }
--- homework_20/20.js
+++ homework_20/20.js
@@ -1,3 +1,7 @@
+'use strict';
+var arr = [];
+var itemsIdCounter = 1;
+
 var myEl = document.getElementById('SubmitForm');
 myEl.onclick =  getFormInput;
 
@@ -11,40 +15,65 @@ function getFormInput () {
     var butt = document.createElement('button');
     var btn = document.createElement('button');
 
+    //buttons
     butt.innerHTML = "Delete";
-    btn.innerHTML = "Change";
+    btn.innerHTML = "Edit";
     butt.setAttribute("class", "btn btn-danger");
     butt.setAttribute( "id", "deleteRow");
     btn.setAttribute("class", "btn btn-primary");
-    btn.setAttribute( "id", "changeRow");
+    btn.setAttribute("id", "editRow");
+    btn.setAttribute( "data-car-item-id", "item.id");
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        var carId = this.getAttribute('data-car-item-id');
+        var carItem = findCarItem(carId);
+        return false;
+    });
 
+    //row data
     var cel1 = newRow.insertCell(0);
     var cel2 = newRow.insertCell(1);
     var cel3 = newRow.insertCell(2);
     var cel4 = newRow.insertCell(3);
     var cel5 = newRow.insertCell(4);
     var cel6 = newRow.insertCell(5);
-    var km = distance.value / 1000;
-    var val = seconds.value / 3600;
-    var cell7 = newRow.insertCell(6);
+    var cel7 = newRow.insertCell(6);
 
-    cel1.innerHTML = carNR.value;
-    cel2.innerHTML = date.value;
-    cel3.innerHTML = distance.value;
-    cel4.innerHTML = seconds.value;
-    cel5.innerHTML = km / val;
+    var carItem = {
+        id: itemsIdCounter,
+        carNumber:carNR.value,
+        date: date.value,
+        distance: distance.value,
+        duration: seconds.value,
+        calcPH: function () {
+            return this.distance / (this.duration / 3600)
+        },
+    };
+    itemsIdCounter++;
+    arr.push(carItem);
+
+    cel1.innerHTML = carItem.carNumber;
+    cel2.innerHTML = carItem.date;
+    cel3.innerHTML = carItem.distance;
+    cel4.innerHTML = carItem.duration;
+    cel5.innerHTML = carItem.calcPH();
     cel6.appendChild(butt);
-    cell7.appendChild(btn);
-
-
+    cel7.appendChild(btn);
 }
-$(document).ready(function(){
-    $('#myTable').on('click', '#deleteRow', function () {
-    $(this).closest('tr').remove();
-    if (confirm('Do you want to remove this?')) {
-    }
-});});
-$(document).ready(function(){
-$('#myTable').on('click', '#changeRow', function open() {
+var tbl = $('#myTable');
+
+    tbl.on('click', '#deleteRow', function () {
+        if (confirm('Do you want to remove this?'))  {
+            $(this).closest('tr').remove();
+            return true;
+        }else
+        {
+            return false;
+        }
+    });
+
+    tbl.on('click', '#editRow', function open() {
+
     $('#exampleModal').modal('show');
-});});
+
+});
